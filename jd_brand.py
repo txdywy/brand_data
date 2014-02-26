21dc3d06b23496b953ae4dbbb8d9a13cfe86b8ca
txdywy <genius.ron@gmail.com>
2014-02-26 16:23:14 +0800

    pull data from jd

---
+++ jd_brand.py
@@ -0,0 +1,41 @@
+from __future__ import with_statement
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8') 
+
+def set_head_row():
+    with open("jd_data.csv",'a') as f:
+        f.write('%s\n'%','.join(["jd_id", "title", "info", "img_url"]))
+
+def get_data_row(url_prefix, id):
+    id = str(id)
+    url = url_prefix%id
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+    
+    #product img url
+    div = soup.find('div',{'id': 'preview'})
+    img = div.find('img')
+    img_url = img.get('src')
+
+    #product info
+    div = soup.find('div',{'class': 'mc',"id":"product-detail-1"})
+    lis = div.findAll('li')
+    title = lis[0].get('title')
+    info = ';'.join([l.getText() for l in lis])
+
+    with open("jd_data.csv",'a') as f:
+        f.write('%s\n'%','.join([id, title, info, img_url]))
+
+url_prefix = "http://item.jd.com/%s.html"
+set_head_row()
+id_base = 650259
+for i in xrange(990,1000):
+    try:
+        id = id_base + i
+        get_data_row(url_prefix, id)
+    except Exception, e:
+        print id, 'shit'
+    print '============>',i, 'done'
