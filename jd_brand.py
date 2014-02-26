878778a693b0aedbc69923f63d9fa3e41117b2e8
Yi Wei <genius.ron@gmail.com>
2014-02-27 01:41:26 +0800

    fetch data from jd.com; throws codec err ones

--- jd_brand.py
+++ jd_brand.py
@@ -1,9 +1,10 @@
 from __future__ import with_statement
 import urllib2
 from BeautifulSoup import BeautifulSoup
+import time
 import sys
 reload(sys)
-sys.setdefaultencoding('utf8') 
+sys.setdefaultencoding('utf-8') 
 
 def set_head_row():
     with open("jd_data.csv",'a') as f:
@@ -24,18 +25,20 @@ def get_data_row(url_prefix, id):
     div = soup.find('div',{'class': 'mc',"id":"product-detail-1"})
     lis = div.findAll('li')
     title = lis[0].get('title')
+    print title
     info = ';'.join([l.getText() for l in lis])
 
     with open("jd_data.csv",'a') as f:
         f.write('%s\n'%','.join([id, title, info, img_url]))
 
 url_prefix = "http://item.jd.com/%s.html"
-set_head_row()
+#set_head_row()
 id_base = 650259
-for i in xrange(990,1000):
+for i in xrange(-710,1000):
     try:
         id = id_base + i
         get_data_row(url_prefix, id)
+        time.sleep(0.1)
     except Exception, e:
-        print id, 'shit'
-    print '============>',i, 'done'
+        print id, 'shit', e
+    print '============>', i, id, 'done'
