aabffa298ea3fe9793c021f0fe99c4e5ad9cba62
txdywy <genius.ron@gmail.com>
2014-03-13 12:14:07 +0800

    more powerful anc acurate data fetch from yhd, all toothpaste
    result file new by ts each time

--- yhd_brand.py
+++ yhd_brand.py
@@ -6,47 +6,68 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8') 
 HEADERS = {'User-Agent':'Mozilla/5.0'}
-
+RESULT_DATA_CSV = 'yhd_data%s.csv'%int(time.time())
 def get_head_row():
     req = urllib2.Request("http://item.yhd.com/item/1810581", None, HEADERS)
     page = urllib2.urlopen(req)
     soup = BeautifulSoup(page)
     l = soup.findAll('input')
-    with open("yhd_data.csv",'a') as f:
+    with open(RESULT_DATA_CSV,'a') as f:
         f.write('%s\n'%','.join([i.get('id') for i in l[1:37]]))
 
 def get_data_row(url):
     req = urllib2.Request(url, None, HEADERS)
-    page = urllib2.urlopen(req)
+    page = urllib2.urlopen(req,None,5.0)
     soup = BeautifulSoup(page)
     l = soup.findAll('input')
     #toothpaste categoryId=5073, l[10]
     if int(l[10].get('value'))==5073:
-        with open("yhd_data.csv",'a') as f:
+        with open(RESULT_DATA_CSV,'a') as f:
             f.write('%s\n'%','.join([i.get('value') for i in l[1:37]]))
         return 1
     return 0
 
-url_prefix = "http://item.yhd.com/item/%s"
-get_head_row()
-id_base = 1000000
-count = 0
-hit = 0
-for i in xrange(1000000):
-    count += 1
-    try:
-        id = id_base + i
-        r = get_data_row(url_prefix%id)
-        if r:
-            hit += 1
-    except Exception, e:
-        #print id, 'shit', e, e.message
-        pass
-    if i%10 == 1:
-        print '%s/%s'%(hit,count)
-    if i%30 == 1:
-        time.sleep(0.3)
-    print '============>',i, 'done'
+URL_PREFIX = "http://item.yhd.com/item/%s"
+def foo():
+    #get_head_row()
+    id_base = 1222620
+    count = 0
+    hit = 0
+    for i in xrange(20000):
+        count += 1
+        try:
+            id = id_base + i
+            r = get_data_row(URL_PREFIX%id)
+            if r:
+                hit += 1
+        except Exception, e:
+            #print id, 'shit', e, e.message
+            pass
+        if i%10 == 1:
+            print '%s/%s'%(hit,count)
+        print '============>',i, 'done', 'id:', id
 
+def get_toothpaste():
+    get_head_row()
+    productIds = []
+    count = 0
+    hit = 0
+    with open("yhd_pt_ids.csv") as f:
+        content = f.readlines()
+        for l in content:
+            l=l.strip('\n').strip('\r')
+            productIds.extend(l.split(','))
+    for id in productIds:
+        count+=1
+        try:
+            r = get_data_row(URL_PREFIX%id)
+            if r:
+               hit += 1
+        except Exception, e:
+            print id, 'shit', e, e.message
+        if count%10 == 1:
+            print '%s/%s'%(hit,count)
+        print '============> done', 'id:', id
 
-			
+get_toothpaste()
+
